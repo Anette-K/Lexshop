6e00eecd99da8e825de80d2500bd093b7609507a
Anette Kniberg <anettekniberg@gmail.com>
2021-01-12 10:59:58 +0100

    Dependency Injection

--- LexShop/LexShop.WebUI/Controllers/ProductManagerController.cs
+++ LexShop/LexShop.WebUI/Controllers/ProductManagerController.cs
@@ -6,18 +6,19 @@ using System.Web.Mvc;
 using LexShop.Core.Models;
 using LexShop.DataAccess.InMemory;
 using LexShop.Core.ViewModels;
+using LexShop.Core.Contracts;
 
 namespace LexShop.WebUI.Controllers
 {
     public class ProductManagerController : Controller
     {
-        InMemoryRepository<Product> context;
-        InMemoryRepository<ProductCategory> productCategories;
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategories;
 
-        public ProductManagerController()
+        public ProductManagerController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext) //injecting through the constr
         {
-            context = new InMemoryRepository<Product>();
-            productCategories = new InMemoryRepository<ProductCategory>();
+            context = productContext;
+            productCategories = productCategoryContext;
         }
         // GET: ProductManager
         public ActionResult Index()
